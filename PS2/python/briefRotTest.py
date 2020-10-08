16b34a59fc9bcb8328a7feb2b3c52614b1ebd19e
zCoCo <connor.caxd@gmail.com>
2020-10-08 08:50:26 -0400

    briefRotTest ready

--- PS2/python/briefRotTest.py
+++ PS2/python/briefRotTest.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
-import skimage.color
 import scipy.ndimage
+import matplotlib.pyplot as plt
 from matchPics import matchPics
-from opts import get_opts
 from helper import plotMatches
+from opts import get_opts
 
 opts = get_opts()
 
@@ -12,21 +12,28 @@ opts = get_opts()
 # Read the image and convert to grayscale, if necessary
 image = cv2.imread('../data/cv_cover.jpg') # Note: matchPics takes care of grayscaling
 
+hist_angles = np.arange(36)*10
+hist_matches = np.zeros((1,36)) # preallocate for speed
+
 for i in range(36):
 	# Rotate Image
-    angle = i*10
+    angle = hist_angles[i]
     rotated = scipy.ndimage.rotate(image, angle)
 	
 	# Compute features, descriptors and Match features
     matches, locs1, locs2 = matchPics(image, rotated, opts)
     
-    if i < 36 and i % (36/3): # Display matching at three orientations
-        print(angle)
-        plotMatches(cv_cover, cv_desk, matches, locs1, locs2)
-
-    i = i
 	# Update histogram
-
+    hist_matches[i] = matches.shape[0]
+    
+    # Display matching at three orientations:
+    if i < 36 and not i % (36/3): 
+        print(angle)
+        plotMatches(image, rotated, matches, locs1, locs2)
 
 # Display histogram
+plt.figure()
+plt.bar(hist_angles, hist_matches)
+plt.show()
 
+pass
