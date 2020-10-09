ec9eaee5fc68f7eb409a0736c54125ed748a084a
zCoCo <connor.caxd@gmail.com>
2020-10-08 22:15:23 -0400

    submission 1  revised

---
+++ PS2/cwcolomb/python/HarryPotterize.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+import skimage.io 
+import skimage.color
+import matplotlib.pyplot as plt
+from opts import get_opts
+
+# Import necessary functions
+from matchPics import matchPics
+from planarH import computeH_ransac
+from planarH import compositeH
+
+opts = get_opts()
+
+# Write script for Q2.2.4
+def q224():
+    # Load images:
+    cv_cover = cv2.imread('../data/cv_cover.jpg')
+    cv_desk = cv2.imread('../data/cv_desk.png')
+    hp_cover = cv2.imread('../data/hp_cover.jpg')
+    
+    # Resize imposed template (hp_cover) to match size of matched template (cv_cover)
+    # (Fix from Q2.2.4.4):
+    hp_cover = cv2.resize(hp_cover, (cv_cover.shape[1], cv_cover.shape[0]))
+    
+    # Compute Homography:
+    matches, locs1, locs2 = matchPics(cv_cover, cv_desk, opts)
+    
+    bestH2to1, inliers = computeH_ransac(locs1[matches[:,0],:], locs2[matches[:,1],:], opts)
+    
+    # Composite Images:
+    composite = compositeH(bestH2to1, hp_cover, cv_desk)
+    
+    # Display Result:
+    plt.figure()
+    plt.imshow(cv2.cvtColor(composite, cv2.COLOR_BGR2RGB))
+    plt.show(block=False)
+    
+    # Save result if address given:
+    if opts.save_addr is not None:
+        plt.savefig(opts.save_addr)
+    
+if opts.c2_testing:
+    # Profile if testing
+    import time
+    t0 = time.perf_counter();
+    q224()
+    print(time.perf_counter() - t0)
+    
+else:
+    q224()
---
+++ PS2/cwcolomb/python/briefRotTest.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+import scipy.ndimage
+import matplotlib.pyplot as plt
+from matchPics import matchPics
+from helper import plotMatches
+from opts import get_opts
+
+opts = get_opts()
+
+# Q2.1.6
+# Read the image and convert to grayscale, if necessary
+image = cv2.imread('../data/cv_cover.jpg') # Note: matchPics takes care of grayscaling
+
+hist_angles = np.arange(36)*10
+hist_matches = np.zeros(36) # preallocate for speed
+
+for i in range(36):
+	# Rotate Image
+    angle = hist_angles[i]
+    rotated = scipy.ndimage.rotate(image, angle)
+	
+	# Compute features, descriptors and Match features
+    matches, locs1, locs2 = matchPics(image, rotated, opts)
+    
+	# Update histogram
+    hist_matches[i] = matches.shape[0]
+    
+    # Display matching at three orientations:
+    if i < 36 and not i % (36/3): 
+        print(angle)
+        plotMatches(image, rotated, matches, locs1, locs2)
+
+# Display histogram
+plt.figure()
+plt.bar(hist_angles, hist_matches)
+plt.show()
+
+pass
