7845a6477748d375b83344ed4a165bfcd0543418
zCoCo <connor.caxd@gmail.com>
2020-10-21 02:15:26 -0400

    q1_2, q1_3, q1_4 done

---
+++ PS3/code/LucasKanade.py
@@ -0,0 +1,63 @@
+import numpy as np
+import numpy.linalg
+import warnings
+from scipy.interpolate import RectBivariateSpline
+
+def LucasKanade(It, It1, rect, threshold, num_iters, p0=np.zeros(2)):
+    """
+    :param It: template image
+    :param It1: Current image
+    :param rect: Current position of the car (top left, bot right coordinates)
+    :param threshold: if the length of dp is smaller than the threshold, terminate the optimization
+    :param num_iters: number of iterations of the optimization
+    :param p0: Initial movement vector [dp_x0, dp_y0]
+    :return: p: movement vector [dp_x, dp_y]
+    """
+
+    # Extract Grid Data:
+    rect = np.asarray(rect).reshape(4) # standardize formatting
+    x1 = rect[0]
+    y1 = rect[1]
+    x2 = rect[2]
+    y2 = rect[3]
+    
+    # Precompute meshgrid (base):
+    x, y = np.meshgrid(np.arange(x1,(x2+1)), np.arange(y1,(y2+1)))
+    x = x.ravel()
+    y = y.ravel()
+    x_int = np.round(x).astype(np.int32) # round to ints for use as indices
+    y_int = np.round(y).astype(np.int32)
+    
+    D = x.size
+    
+    # Precompute Spline Functor:
+    It1_spline = RectBivariateSpline(range(It1.shape[1]),range(It1.shape[0]), It1.T)
+
+    # Solve with Lucas-Kanade:
+    p = p0
+    
+    iter_count = 0
+    while True:
+        A = np.zeros((D,2))
+        b = np.zeros((D,1))
+        
+        xp = x + p[0] # all (xi+px)
+        yp = y + p[1] # all (yi+py)
+        A[:,0] = It1_spline(xp,yp, dx=1, grid=False) # Ix
+        A[:,1] = It1_spline(xp,yp, dy=1, grid=False)# Iy
+        b = It[y_int,x_int] - It1_spline(xp,yp, grid=False)
+        
+        eigs, _ = np.linalg.eig(A.T@A)
+        if not np.all(eigs / np.max(eigs) > 1e-6):
+            warnings.warn("A is singular or ill-conditioned with eigs: {}".format(eigs))
+            
+        # Update:
+        Dp = np.linalg.inv(A.T@A) @ A.T @ b
+        p = p + Dp
+        
+        # Check Termination Conditions3:
+        iter_count = iter_count + 1
+        if np.sum(Dp**2) < threshold or iter_count > num_iters:
+            break
+
+    return p
---
+++ PS3/code/testGirlSequence.py
@@ -0,0 +1,43 @@
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from LucasKanade import LucasKanade
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_iters', type=int, default=1e4, help='number of iterations of Lucas-Kanade')
+parser.add_argument('--threshold', type=float, default=1e-4, help='dp threshold of Lucas-Kanade for terminating optimization')
+args = parser.parse_args()
+num_iters = args.num_iters
+threshold = args.threshold
+
+seq = np.load("../data/girlseq.npy")
+num_frames = seq.shape[2];
+    
+rect = [280, 152, 330, 318] # initial rectangle value
+
+rects = np.zeros((num_frames,4))
+rects[0,:] = rect
+
+def show_frame(f, save=False):
+    plt.figure()
+    plt.imshow(seq[:,:,f], cmap='gray')
+    width, height = rects[f,2:] - rects[f,0:2]
+    patch = patches.Rectangle(rects[f,0:2], width, height, linewidth=1, edgecolor='r', facecolor='none')
+    plt.gca().add_patch(patch)
+    if save:
+        plt.axis('off')
+        plt.savefig("../outputs/q1_3_girl_{}.png".format(f), bbox_inches = 'tight')
+    else:
+        plt.show()
+
+for f in range(num_frames-1):
+    # NB: argmin is init w/zeros (better performance than p_n-1/minimizes affect of drift - prevents inertia)
+    p = LucasKanade(seq[:,:,f], seq[:,:,f+1], rects[f,:], threshold, num_iters, p0=np.zeros(2))
+    rects[f+1,:] = rects[f,:] + [p[0],p[1], p[0],p[1]]
+    #print("{}/{}".format(f+2,num_frames))
+
+np.save('../result/girlseqrects.npy', rects) # Note: rect is allowed to be float according to Piazza @323
+
+for f in [1,20,40,60,80]:
+    show_frame(f)
